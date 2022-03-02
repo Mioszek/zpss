a973b8bc17bf1eaf1a69b80109e6a3da9bb777d3
Mioszek <228871@edu.p.lodz.pl>
2022-03-02 15:34:03 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/zpss.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zpss.dir/main.cpp.obj"
+  "libzpss.dll.a"
+  "zpss.exe"
+  "zpss.exe.manifest"
+  "zpss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zpss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/zpss.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for zpss.
+# This may be replaced when dependencies are built.
